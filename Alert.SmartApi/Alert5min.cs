250e94533954ab863e259c2e4a97a3b61386daa1
Pawan Singh <pwn.singh14@gmail.com>
2022-10-31 10:05:09 +0530

    .

--- Alert.SmartApi/Alert5min.cs
+++ Alert.SmartApi/Alert5min.cs
@@ -8,15 +8,16 @@ namespace Alert.SmartApi
 {
     public class Alert5min
     {
-        [FunctionName("Alert5min")]
-        public void Run([TimerTrigger("0 1/5 4-10 * * 1-5")] TimerInfo myTimer, ILogger log)
-        {
-            DateTime dt = DateTime.UtcNow.ToIstDateTime();
-            var connect = new AngelBroking.SmartApi(Constant.api_key, "", "");
-            connect.GenerateSession(Constant.client_code, Constant.password, connect.GetTotp());
-            connect.GenerateToken();
+        //[Disable("Alert5min")]
+        //[FunctionName("Alert5min")]
+        //public void Run([TimerTrigger("0 1/5 4-10 * * 1-5")] TimerInfo myTimer, ILogger log)
+        //{
+        //    DateTime dt = DateTime.UtcNow.ToIstDateTime();
+        //    var connect = new AngelBroking.SmartApi(Constant.api_key, "", "");
+        //    connect.GenerateSession(Constant.client_code, Constant.password, connect.GetTotp());
+        //    connect.GenerateToken();
 
-            _2.ReportUnusualChanges(dt, connect);
-        }
+        //    _2.ReportUnusualChanges(dt, connect);
+        //}
     }
 }
